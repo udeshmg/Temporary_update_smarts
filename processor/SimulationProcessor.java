867aac7419af91de8645c37ce18d6443aceef915
tmuthugama <tmuthugama@student.unimelb.edu.au>
2019-03-15 15:28:27 +1100

    Introduce new interface SimulationProcessor

---
+++ processor/SimulationProcessor.java
@@ -0,0 +1,35 @@
+package processor;
+
+import traffic.road.Node;
+import traffic.road.RoadNetwork;
+
+/**
+ * Copyright (c) 2019, The University of Melbourne.
+ * All rights reserved.
+ * <p>
+ * You are permitted to use the code for research purposes provided that the following conditions are met:
+ * <p>
+ * * You cannot redistribute any part of the code.
+ * * You must give appropriate citations in any derived work.
+ * * You cannot use any part of the code for commercial purposes.
+ * * The copyright holder reserves the right to change the conditions at any time without prior notice.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+ * USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * <p>
+ * Created by tmuthugama on 3/15/2019
+ */
+public interface SimulationProcessor {
+
+    RoadNetwork getRoadNetwork();
+    void stopSim();
+    void resumeSim();
+    void pauseSim();
+    void askWorkersChangeLaneBlock(int laneIndex, boolean block);
+    void setLightChangeNode(Node nodeSelected);
+}
