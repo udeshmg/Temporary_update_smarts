23a99548934be09831ba0e37f0b72e030973da17
tmuthugama <tmuthugama@student.unimelb.edu.au>
2019-07-30 15:46:16 +1000

    Set route output file an xml file

--- processor/server/FileOutput.java
+++ processor/server/FileOutput.java
@@ -91,7 +91,7 @@ public class FileOutput {
 
 	void initRouteOutputFile() {
 		try {
-			final File file = getNewFile(Settings.prefixOutputRoutePlan);
+			final File file = getNewFile(Settings.prefixOutputRoutePlan, "xml");
 			fosRoute = new FileOutputStream(file, true);
 			outputStringToFile(fosRoute,
 					"<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + System.getProperty("line.separator"));
